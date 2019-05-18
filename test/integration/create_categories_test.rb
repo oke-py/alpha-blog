b647fc9788d0076ea64615641e621af551bbcad6
Naoki Oketani <okepy.naoki@gmail.com>
2019-05-18 01:27:15 +0000

    Integration Test for Invalid Category

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -8,9 +8,19 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
         assert_difference 'Category.count', 1 do
             post categories_path, params: { category: { name: "sports" } }
             follow_redirect!
-            
-            assert_template 'categories/index'
-            assert_match "sports", response.body
         end
+        assert_template 'categories/index'
+        assert_match "sports", response.body
+    end
+    
+    test "invalid category submission results in failure" do
+        get new_category_path
+        assert_template 'categories/new'
+        assert_no_difference 'Category.count' do
+            post categories_path, params: { category: { name: " " } }
+        end
+        assert_template 'categories/new'
+        assert_select 'h2.panel-title'
+        assert_select 'div.panel-body'
     end
 end
